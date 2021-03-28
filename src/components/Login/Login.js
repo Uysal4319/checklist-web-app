2c96de7fd7678945ed54136d32b88fd8bb6d0e55
ertugruluy <ertugrul.uysal@argela.com.tr>
2021-03-28 14:52:26 +0300

    loading eklendi

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Typing from "../Typing";
 import { withRouter } from 'react-router-dom';
+import ReactLoading from "react-loading";
 
 
 class Login extends Component {
@@ -10,7 +11,8 @@ class Login extends Component {
         this.state = {
             user: '',
             password :'',
-            passed: false
+            passed: false,
+            visible: false
         }
         this.goLogin = this.goLogin.bind(this);
     }
@@ -21,7 +23,7 @@ class Login extends Component {
         //
         // console.error("user: " + user + " pass : "+ password);
 
-
+         this.setState({ visible : true})
              fetch('https://spring-eu.herokuapp.com/authenticate', {
                  method: 'POST',
                  headers :{
@@ -35,7 +37,7 @@ class Login extends Component {
                  .then((res)=>res.json())
                  .then((res) => {
 
-
+                     this.setState({ visible : false})
                      this.props.history.push("/home");
 
                      console.error("Token : " + res.token);
@@ -70,25 +72,39 @@ class Login extends Component {
 
 
     render() {
-        return (
-            <div>
-                <Typing
-                    placeHolder = "User"
-                    onChangeText = { value =>{this.setState({user: value.target.value})}}
-                    value = {this.state.user}
-                />
+        if(!this.state.loading){
+            return (
+                <div>
+                    <Typing
+                        placeHolder = "User"
+                        onChangeText = { value =>{this.setState({user: value.target.value})}}
+                        value = {this.state.user}
+                    />
 
-                <Typing
-                    placeHolder = "Password"
-                    onChangeText = {value =>  {this.setState({password: value.target.value})}}
-                    value = {this.state.password}
-                />
+                    <Typing
+                        placeHolder = "Password"
+                        onChangeText = {value =>  {this.setState({password: value.target.value})}}
+                        value = {this.state.password}
+                    />
 
-                <button className={'login'} onClick={this.goLogin}> LoginPage! </button>
+                    <button className={'login'} onClick={this.goLogin}> LoginPage! </button>
 
-                {/*<NavLink className={'login'} to="/home" >Login!</NavLink>*/}
-            </div>
-        );
+                    {/*<NavLink className={'login'} to="/home" >Login!</NavLink>*/}
+                </div>
+            );
+        } else {
+
+            return (
+                <div  style={{
+                    position: 'absolute', left: '50%', top: '50%',
+                    transform: 'translate(-50%, -50%)'
+                }}>
+                    <ReactLoading className={'loading'} type="bars" color="gray" />
+                </div>
+            );
+
+        }
+
     }
 }
 
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,7 +3,6 @@ import React from "react";
 import Header from "../components/Header/Header";
 import Login from "../components/Login/Login";
 
-
 function LoginPage() {
     return (
         <div className="App">
